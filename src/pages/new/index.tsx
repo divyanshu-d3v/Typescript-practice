18125897d5b646622464e75a4623e04b49ce475d
Divyanshu <divyanshucodes@outlook.com>
2024-12-20 15:26:29 +0530

    initial commit

---
+++ src/pages/new/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NoteForm from "@/components/NoteForm";
+
+const New: React.FC = () => {
+    return (
+        <>
+            <h1>New page</h1>
+            <NoteForm />
+        </>
+    )
+}
+export default New
